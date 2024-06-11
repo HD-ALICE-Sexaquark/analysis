9deae1ff0736b5dbe16cf11e9246b6bab420c754
A. Bórquez <andres.borquezc@gmail.com>
2024-06-11 23:52:47 +0200

    macros: cont. testing blastwave shape, now with sexaquark params

--- macros/BlastWave.cxx
+++ macros/BlastWave.cxx
@@ -10,16 +10,6 @@
 #include "Math/Integrator.h"
 
 /*
-void BlastWave() {
-    Int_t n = 2;
-    auto myFunction = [&n](Double_t x) { return TMath::Power(x, n); };
-    ROOT::Math::Functor1D functor(myFunction);
-    ROOT::Math::Integrator integrator(functor);
-    std::cout << "integral: " << integrator.Integral(0., 1.) << std::endl;
-}
-*/
-
-/*
   Define blast-wave function.
 */
 Double_t BlastWaveFcn(Double_t *x, Double_t *par) {
@@ -43,40 +33,48 @@ Double_t BlastWaveFcn(Double_t *x, Double_t *par) {
     ROOT::Math::Functor1D blastWaveIntFunctor(BlastWaveIntegrand);
     ROOT::Math::Integrator integrator(blastWaveIntFunctor);
 
-    return pt * mt * integrator.Integral(0., 1.);
+    // return pt * mt * integrator.Integral(0., 1.);
+    return mt * integrator.Integral(0., 1.);
 }
 
 void BlastWave() {
 
-    Float_t min_pt = 0.1;
-    Float_t max_pt = 3.;
+    Float_t min_pt = 0.;
+    Float_t max_pt = 5.;
+    Float_t m0 = 1.73;     // mass of the anti-sexaquark (in GeV/c^2)
+    Float_t Tkin = 0.107;  // kinetic freeze-out temperature (in GeV)
+    Float_t beta_s = 0.6;  // transverse velocity at the surface
+    Float_t R = 1.;        // radius of the source
+    Float_t np = 1.;       // radial flow power
 
-    Float_t m0 = 0.938;  // mass of the proton (in GeV/c^2)
-    //   Float_t Tkin = 0.107; // kinetic freeze-out temperature (in GeV)
-    //   Float_t beta_s = 0.8; // transverse velocity at the surface
-    Float_t R = 1.;   // radius of the source
-    Float_t np = 1.;  // radial flow power
+    /*
+    // debug
+        Double_t par[5] = {0.938, 0.1, 0.8, 1., 1.};
+    for (Double_t pt = 0.; pt < 5.; pt += 0.5) {
+        std::cout << pt << " " << BlastWaveFcn(&pt, par) << std::endl;
+    }
+    */
 
     TF1 *f = new TF1("f", BlastWaveFcn, min_pt, max_pt, 5);
     f->SetParameter(0, m0);
-    f->SetParameter(1, 0.1);
-    f->SetParameter(2, 0.2);
+    f->SetParameter(1, Tkin);
+    f->SetParameter(2, beta_s);
     f->SetParameter(3, R);
     f->SetParameter(4, np);
 
     f->SetLineColor(kBlue);
 
-    TF1 *f2 = new TF1("f2", BlastWaveFcn, min_pt, max_pt, 5);
-    f2->SetParameter(0, m0);
-    f2->SetParameter(1, 0.1);
-    f2->SetParameter(2, 0.8);
-    f2->SetParameter(3, R);
-    f2->SetParameter(4, np);
+    // TF1 *f2 = new TF1("f2", BlastWaveFcn, min_pt, max_pt, 5);
+    // f2->SetParameter(0, m0);
+    // f2->SetParameter(1, 0.1);
+    // f2->SetParameter(2, 0.8);
+    // f2->SetParameter(3, R);
+    // f2->SetParameter(4, np);
 
-    f2->SetLineColor(kRed);
+    // f2->SetLineColor(kRed);
 
     TCanvas *c = new TCanvas("c", "c", 720, 720);
 
     f->Draw();
-    f2->Draw("SAME");
+    // f2->Draw("SAME");
 }
