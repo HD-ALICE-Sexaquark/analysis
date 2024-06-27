7e7bd6da440f6785a9e40a87279d7ce992692c0f
A. Bórquez <andres.borquezc@gmail.com>
2024-06-27 23:24:04 +0200

    macros: updated HistogramContents macro - to new lists structure

--- macros/Test_HistogramContents.C
+++ macros/Test_HistogramContents.C
@@ -14,39 +14,45 @@ Int_t Test_HistogramContents(TString filename) {
 
     /* Get the list of histograms */
 
-    TList* histList = dynamic_cast<TList*>(file->Get("Hists"));
-    if (!histList) {
-        printf("Error retrieving histogram list\n");
-        file->Close();
-        return 1;
-    }
+    std::vector<TString> hist_containers = {"QA_Hists", "Tracks_Hists", "V0s_Hists", "Sexaquarks_Hists", "PosKaonPairs_Hists"};
 
-    /* Loop over the histograms */
+    for (TString& hist_ctn : hist_containers) {
 
-    TIter next(histList);
-    TObject* obj;
-
-    Int_t n_entries;
-    Int_t count_empty = 0;
-
-    printf("%-45s %10s\n", "Name", "N_Entries");
-    printf("%-45s %10s\n", "====", "=========");
-    while ((obj = next())) {
-        TH1* hist = dynamic_cast<TH1*>(obj);
-        Int_t n_entries = (Int_t)hist->GetEntries();
-        /* Note for myself: */
-        /* - `\033[1;31m` is the ANSI escape code for red and bold */
-        /* - `\033[0m` is the ANSI escape code to reset color and style to default */
-        if (n_entries == 0) {
-            printf("%-45s \033[1;31m%10i\033[0m\n", hist->GetName(), n_entries);
-            count_empty++;
-        } else {
-            printf("%-45s %10i\n", hist->GetName(), n_entries);
+        TList* histList = dynamic_cast<TList*>(file->Get(hist_ctn));
+        if (!histList) {
+            printf("Error retrieving histogram list %s\n", hist_ctn.Data());
+            file->Close();
+            return 1;
         }
+
+        /* Loop over the histograms */
+
+        TIter next(histList);
+        TObject* obj;
+
+        Int_t n_entries;
+        Int_t count_empty = 0;
+
+        printf("# %s\n\n", hist_ctn.Data());
+        printf("%-45s %10s\n", "Name", "N_Entries");
+        printf("%-45s %10s\n", "====", "=========");
+        while ((obj = next())) {
+            TH1* hist = dynamic_cast<TH1*>(obj);
+            Int_t n_entries = (Int_t)hist->GetEntries();
+            /* Note for myself: */
+            /* - `\033[1;31m` is the ANSI escape code for red and bold */
+            /* - `\033[0m` is the ANSI escape code to reset color and style to default */
+            if (n_entries == 0) {
+                printf("%-45s \033[1;31m%10i\033[0m\n", hist->GetName(), n_entries);
+                count_empty++;
+            } else {
+                printf("%-45s %10i\n", hist->GetName(), n_entries);
+            }
+        }
+        printf("%56s\n", "==========");
+        printf("%45s %10i\n", "N_Histograms =", histList->GetEntries());
+        printf("%45s %10i\n\n", "N_Empty_Histograms =", count_empty);
     }
-    printf("%51s\n", "==========");
-    printf("%45s %10i\n", "N_Histograms =", histList->GetEntries());
-    printf("%45s %10i\n", "N_Empty_Histograms =", count_empty);
 
     file->Close();
 
