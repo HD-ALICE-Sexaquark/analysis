b0913c36d8ec91d012c29dacf551750540b386c7
A. Bórquez <andres.borquezc@gmail.com>
2024-09-10 22:32:00 +0200

    merge macro: now merges hists - found bug when merging trees, investigating...

--- macros/OutputFiles_Merge.C
+++ macros/OutputFiles_Merge.C
@@ -1,32 +1,32 @@
 #include "include/Headers.hxx"
 
-void OutputFiles_Merge(TString InputDir, TString OutputFilename = "AnalysisResults_merged.root") {
+#include "../sexaquark/AliAnalysisTaskSexaquark_Structs.h"
+
+void OutputFiles_Merge(TString InputDir, TString OutputFilename = "AnalysisResults_merged2.root") {
 
     TSystemDirectory dir(InputDir, InputDir);
     TList *files = dir.GetListOfFiles();
 
-    /* Define General Properties */
+    std::cout << "OutputFiles_Merge :: Merging " << files->GetEntries() << " files into " << OutputFilename << "..." << std::endl;
 
-    std::vector<TString> StrInputFiles;
+    /* Define General Properties */
 
     TIter next(files);
     TString StrList_Trees = "Trees";
-    // TString StrList_QAHists = "QAHists";
-    // TString StrList_TrackHists = "Track_Hists";
-    // TString StrList_V0sHists = "V0s_Hists";
-    // TString StrList_SexaquarksHists = "Sexaquarks_Hists";
-    // TString StrList_PosKaonPairsHists = "PosKaonPairs_Hists";
+    std::vector<TString> Str_Chains = {"Injected",    "Events",      "MCParticles",    "PiPluses",  "PiMinuses",
+                                       "AntiProtons", "AntiLambdas", "KaonsZeroShort", "Sexaquarks"};
+    std::vector<TString> StrList_Hists = {"QA_Hists", "Tracks_Hists", "V0s_Hists", "Sexaquarks_Hists", "PosKaonPairs_Hists"};
 
     /* Create Output File */
 
     TFile *OutputFile = new TFile(OutputFilename, "RECREATE");
 
     TList *OutputList_Trees = new TList();
-    // TList *List_QAHists = new TList();
-    // TList *List_TrackHists = new TList();
-    // TList *List_V0sHists = new TList();
-    // TList *List_SexaquarksHists = new TList();
-    // TList *List_PosKaonPairsHists = new TList();
+    std::vector<TChain *> Output_Chains;
+    for (Int_t tt = 0; tt < (Int_t)Str_Chains.size(); tt++) Output_Chains.push_back(new TChain(Str_Chains[tt]));  // TO CONTINUE
+
+    std::vector<TList *> OutputList_Hists;
+    for (Int_t hh = 0; hh < (Int_t)StrList_Hists.size(); hh++) OutputList_Hists.push_back(new TList());
 
     /* Define Properties of Current File */
 
@@ -35,18 +35,20 @@ void OutputFiles_Merge(TString InputDir, TString OutputFilename = "AnalysisResul
     TFile *InputFile;
 
     TList *InputList_Trees;
+    TList *InputList_QAHists;
 
-    Int_t FileCounter = 0;
+    // loop over files
     while ((file = (TSystemFile *)next())) {
-        if (FileCounter > 1) break;
+
+        // get index of file within the TList
+        Int_t index = files->IndexOf(file);
+        if (index > 2) break;
+        std::cout << "OutputFiles_Merge :: Processing file " << index << " of " << files->GetEntries() << "..." << std::endl;
 
         filename = file->GetName();
         filepath = InputDir + "/" + filename;
 
         if (!filename.Contains("AnalysisResults_")) continue;
-
-        std::cout << "file: " << filename << std::endl;
-        StrInputFiles.push_back(filename);
 
         InputFile = new TFile(filepath, "READ");
 
@@ -54,33 +56,60 @@ void OutputFiles_Merge(TString InputDir, TString OutputFilename = "AnalysisResul
 
         InputList_Trees = (TList *)InputFile->Get(StrList_Trees);
 
+        // loop over trees
         TIter nextTree(InputList_Trees);
-        TTree *obj;
-
-        while ((obj = (TTree *)nextTree())) {
+        TTree *auxTree;
+        while ((auxTree = (TTree *)nextTree())) {
+            TString treeName = auxTree->GetName();
             OutputFile->cd();
-            if (FileCounter == 0) {
-                // add it
-                TTree *newTree = obj->CloneTree();
+            if (!OutputList_Trees->FindObject(treeName)) {
+                // if doesn't exist, clone it
+                TTree *newTree = auxTree->CloneTree();
                 OutputList_Trees->Add(newTree);
             } else {
-                // merge tree of the same name
-                TTree *oldTree = (TTree *)OutputList_Trees->FindObject(obj->GetName());
+                // if already exists, merge tree
+                TTree *oldTree = (TTree *)OutputList_Trees->FindObject(treeName);
                 TList *auxList = new TList();
-                auxList->Add(obj);
+                auxList->Add(auxTree);
                 oldTree->Merge(auxList);
                 delete auxList;
             }
         }
 
-        FileCounter++;
+        /* Histograms */
+
+        // loop over histograms lists
+        TList *OutputList;
+        for (Int_t hh = 0; hh < (Int_t)StrList_Hists.size(); hh++) {
+
+            OutputList = OutputList_Hists[hh];
+            InputList_QAHists = (TList *)InputFile->Get(StrList_Hists[hh]);
+
+            TIter nextHist(InputList_QAHists);
+            TObject *auxObj;
+
+            // loop over hists
+            while ((auxObj = (TObject *)nextHist())) {
+                OutputFile->cd();
+                if (!OutputList->FindObject(auxObj->GetName())) {
+                    // if doesn't exist, clone it
+                    TObject *newHist = auxObj->Clone();
+                    OutputList->Add(newHist);
+                } else {
+                    // if already exists, add hist
+                    TH1 *oldHist = (TH1 *)OutputList->FindObject(auxObj->GetName());
+                    TH1 *newHist = (TH1 *)auxObj;
+                    oldHist->Add(newHist);
+                }
+            }
+        }
+
         InputFile->Close();
     }
 
-    std::cout << "Merging " << StrInputFiles.size() << " files!" << std::endl;
-
     OutputFile->cd();
     OutputList_Trees->Write("Trees", TObject::kSingleKey);
+    for (Int_t hh = 0; hh < (Int_t)StrList_Hists.size(); hh++) OutputList_Hists[hh]->Write(StrList_Hists[hh], TObject::kSingleKey);
 
     OutputFile->Close();
 }
