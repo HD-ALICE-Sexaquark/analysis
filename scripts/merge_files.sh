5a710f3513a34a3f6d74b3ac63ad6e2de4384493
A. Bórquez <andres.borquezc@gmail.com>
2024-09-27 23:46:56 +0200

    scripts: simplified merge files - it now uses alihadd

--- scripts/merge_files.sh
+++ scripts/merge_files.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 FILES_PATH=$1
-RN_LIST=$2
 
-mkdir -p ${FILES_PATH}/merged
 echo "Merging files in ${FILES_PATH}"
 
-while read -r rn; do
-    echo "Merging ${rn}"
-    hadd -f -k -O ${FILES_PATH}/merged/AnalysisResults_${rn}.root ${FILES_PATH}/AnalysisResults_${rn}_*.root &>> ${FILES_PATH}/merged/merging_rn.log
-done < ${RN_LIST}
+alihadd -O -s 500000000 -k ${FILES_PATH}/AnalysisResults_merged.root ${FILES_PATH}/AnalysisResults_*.root &> ${FILES_PATH}/merging.log
