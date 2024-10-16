466e494a3c794cce1cd2dd19ed7f3ca9951c4a24
A. Bórquez <andres.borquezc@gmail.com>
2024-10-16 23:55:44 +0200

    macros: debugging BuildIndices - MCParticles throws errors - maybe other methods are needed?

--- macros/Trees_BuildIndices.C
+++ macros/Trees_BuildIndices.C
@@ -5,30 +5,21 @@
  * Build primary and secondary indices for each tree.
  * Save the indexed trees to a new output file with `_indexed` appended to the original filename.
  */
-void Trees_BuildIndices(TString InputFileName = "AnalysisResults.root", TString ReactionLetter = "A") {
+void Trees_BuildIndices(TString InputFileName = "../output/A1.8_18qr_local/AnalysisResults_merged.root") {
 
     TFile* InputFile = TFile::Open(InputFileName, "READ");
     if (!InputFile || InputFile->IsZombie()) {
-        std::cout << "!! ERROR !! Couldn't open file " << InputFileName << " !!" << std::endl;
-        return;
-    }
-
-    /* Get Trees List */
-
-    TString TreesListName = "Trees";
-    TList* TreesList = (TList*)InputFile->Get(TreesListName);
-    if (!TreesList) {
-        std::cout << "!! ERROR !! Couldn't find TList " << TreesListName << " in " << InputFileName << " !!" << std::endl;
-        InputFile->Close();
+        std::cout << "!! ERROR !! Trees_BuildIndices !! Couldn't open TFile " << InputFileName << " !!" << std::endl;
         return;
     }
 
     /* Open Output File */
 
-    TString OutputFileName = InputFileName.ReplaceAll(".root", "_indexed.root");
+    TString OutputFileName = InputFileName;
+    OutputFileName.ReplaceAll("_merged.root", "_indexed.root");
     TFile* OutputFile = TFile::Open(OutputFileName, "RECREATE");
     if (!OutputFile) {
-        std::cout << "!! ERROR !! Couldn't create file " << OutputFileName << " !!" << std::endl;
+        std::cout << "!! ERROR !! Trees_BuildIndices !! Couldn't create TFile " << OutputFileName << " !!" << std::endl;
         InputFile->Close();
         return;
     }
@@ -47,54 +38,50 @@ void Trees_BuildIndices(TString InputFileName = "AnalysisResults.root", TString 
     TTree* InputEventsTree;
     TTree* NewEventsTree;
 
-    std::vector<TString> TreeNames = {"Injected", "Events", "MCParticles", "Sexaquarks", "AntiLambdas", "AntiProtons", "PiPluses"};
-    if (ReactionLetter == "A") {
-        TreeNames.push_back("KaonsZeroShort");
-        TreeNames.push_back("PiMinuses");
-    } else if (ReactionLetter == "D") {
-        TreeNames.push_back("PosKaons");
-    } else if (ReactionLetter == "E") {
-        TreeNames.push_back("PionPairs");
-        TreeNames.push_back("PiMinuses");
-        TreeNames.push_back("PosKaons");
-    }
+    std::vector<TString> TreeNames = {"Injected", "Events", "MCParticles", "Tracks", "V0s", "Sexaquarks"};
+    // std::vector<TString> TreeNames = {"Tracks", "V0s", "Sexaquarks"};
+    // std::vector<TString> TreeNames = {"MCParticles"};
 
     for (Int_t i = 0; i < (Int_t)TreeNames.size(); i++) {
-        InputEventsTree = (TTree*)TreesList->FindObject(TreeNames[i]);
+        InputEventsTree = InputFile->Get<TTree>(TreeNames[i]);
         if (!InputEventsTree) {
-            std::cout << "!! ERROR !! Couldn't find TTree " << TreeNames[i] << " in " << TreesListName << " !!" << std::endl;
+            std::cout << "!! ERROR !! Trees_BuildIndices !! Couldn't find TTree " << TreeNames[i] << " in TFile " << InputFileName << " !!"
+                      << std::endl;
             InputFile->Close();
             OutputFile->Close();
             return;
         }
-        // std::cout << "!! INFO !! Input TTree " << TreeNames[i] << " has " << InputEventsTree->GetEntries() << " entries !!" << std::endl; // DEBUG
+        std::cout << "!! INFO  !! Trees_BuildIndices !! Input TTree " << TreeNames[i] << " has " << InputEventsTree->GetEntries() << " entries !!"
+                  << std::endl;  // DEBUG
 
-        OutputFile->cd();
-
-        NewEventsTree = (TTree*)InputEventsTree->CloneTree();
+        NewEventsTree = InputEventsTree->CloneTree();
         if (!NewEventsTree) {
-            std::cout << "!! ERROR !! Failed to clone TTree " << TreeNames[i] << " !!" << std::endl;
+            std::cout << "!! ERROR !! Trees_BuildIndices !! Failed to clone TTree " << TreeNames[i] << " !!" << std::endl;
             InputFile->Close();
             OutputFile->Close();
             return;
         }
-        // std::cout << "!! INFO !! Cloned TTree " << TreeNames[i] << " has " << NewEventsTree->GetEntries() << " entries !!" << std::endl; // DEBUG
+        NewEventsTree->SetDirectory(0);
+
+        std::cout << "!! INFO  !! Trees_BuildIndices !! Cloned TTree " << TreeNames[i] << " has " << NewEventsTree->GetEntries() << " entries !!"
+                  << std::endl;  // DEBUG
 
         MinorIndex = MinorIndex_General;
         if (TreeNames[i] == "Sexaquarks" || TreeNames[i] == "Injected") MinorIndex = MinorIndex_Sexaquark;
         if (TreeNames[i] == "Events") MinorIndex = MinorIndex_Event;
 
         NewEventsTree->BuildIndex(MajorIndex, MinorIndex);
-        std::cout << "!! INFO !! Indexing complete for TTree " << NewEventsTree->GetName() << " !!" << std::endl;
+        std::cout << "!! INFO  !! Trees_BuildIndices !! Indexing complete for TTree " << NewEventsTree->GetName() << " !!" << std::endl;
 
         NewEventsTree->Write();
-        // std::cout << "!! INFO !! Indexed TTree " << NewEventsTree->GetName() << " stored in " << OutputFileName << " !!" << std::endl; // DEBUG
+        std::cout << "!! INFO  !! Trees_BuildIndices !! Indexed TTree " << NewEventsTree->GetName() << " stored in TFile " << OutputFileName << " !!"
+                  << std::endl;  // DEBUG
     }
 
     OutputFile->Close();
     InputFile->Close();
 
-    std::cout << "!! INFO !! All new trees have been stored in " << OutputFileName << " !!" << std::endl;
+    std::cout << "!! INFO  !! Trees_BuildIndices !! All new trees have been stored in TFile " << OutputFileName << " !!" << std::endl;
 
     return;
 }
