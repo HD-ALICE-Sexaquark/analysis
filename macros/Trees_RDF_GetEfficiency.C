28e57f458c9a187ec756717658e98b655b794864
A. Bórquez <andres.borquezc@gmail.com>
2024-10-18 20:51:13 +0200

    macros, get eff.: added true radius distr.

--- macros/Trees_RDF_GetEfficiency.C
+++ macros/Trees_RDF_GetEfficiency.C
@@ -3,35 +3,39 @@
 
 using namespace ROOT;
 
-const Int_t N_CORES = 48;
+// const Int_t N_CORES = 48;
 
 void Trees_RDF_GetEfficiency(TString InputFileName = "../output/A1.8_18qr_local/AnalysisResults_merged.root",  //
-                             TString OutputFileName = "output_rdf/EfficiencyResults.root",                     //
+                             TString OutputFileName = "output_rdf/EfficiencyResults_RDFnHT.root",              //
                              TString PhotonConvRadiusPath = "../radius-weights/output_bkg/merged.root",        //
                              TString BlastWavePath = "blastwave_bt.root") {
 
     /* Input */
 
-    EnableImplicitMT(N_CORES);
+    // EnableImplicitMT(N_CORES);
 
     RDataFrame RDF_Events("Events", InputFileName);
-    // RDataFrame RDF_MCParticles("MCParticles", InputFileName);
+    RDataFrame RDF_MCParticles("MCParticles", InputFileName);
     RDataFrame RDF_Injected("Injected", InputFileName);
     RDataFrame RDF_Sexaquarks("Sexaquarks", InputFileName);
+
+    auto fRDF_MCParticles = RDF_MCParticles.Filter("IsSignal").Filter("Idx_Mother == -1");
+
+    /** Hash tables **/
 
     std::vector<Int_t> Events_RunNumber = RDF_Events.Take<Int_t>("RunNumber").GetValue();
     std::vector<Int_t> Events_DirNumber = RDF_Events.Take<Int_t>("DirNumber").GetValue();
     std::vector<Int_t> Events_EventNumber = RDF_Events.Take<Int_t>("EventNumber").GetValue();
     std::vector<Float_t> Events_Centrality = RDF_Events.Take<Float_t>("Centrality").GetValue();
 
-    // std::vector<Int_t> MCParticles_RunNumber = RDF_MCParticles.Take<Int_t>("RunNumber").GetValue();
-    // std::vector<Int_t> MCParticles_DirNumber = RDF_MCParticles.Take<Int_t>("DirNumber").GetValue();
-    // std::vector<Int_t> MCParticles_EventNumber = RDF_MCParticles.Take<Int_t>("EventNumber").GetValue();
-    // std::vector<Int_t> MCParticles_ReactionID = RDF_MCParticles.Take<Int_t>("ReactionID").GetValue();
-    // std::vector<Float_t> MCParticles_Xv_i = RDF_MCParticles.Take<Float_t>("Xv_i").GetValue();
-    // std::vector<Float_t> MCParticles_Yv_i = RDF_MCParticles.Take<Float_t>("Yv_i").GetValue();
+    std::vector<Int_t> MCParticles_RunNumber = fRDF_MCParticles.Take<Int_t>("RunNumber").GetValue();
+    std::vector<Int_t> MCParticles_DirNumber = fRDF_MCParticles.Take<Int_t>("DirNumber").GetValue();
+    std::vector<Int_t> MCParticles_EventNumber = fRDF_MCParticles.Take<Int_t>("EventNumber").GetValue();
+    std::vector<Int_t> MCParticles_ReactionID = fRDF_MCParticles.Take<Int_t>("ReactionID").GetValue();
+    std::vector<Float_t> MCParticles_Xv_i = fRDF_MCParticles.Take<Float_t>("Xv_i").GetValue();
+    std::vector<Float_t> MCParticles_Yv_i = fRDF_MCParticles.Take<Float_t>("Yv_i").GetValue();
 
-    /* Make hash tables for Centrality, that use RunNumber,DirNumber,EventNumber as keys */
+    /* Centrality */
 
     std::map<std::tuple<Int_t, Int_t, Int_t>, Float_t> Hash_Centrality;
     for (Int_t i = 0; i < (Int_t)Events_RunNumber.size(); i++) {
@@ -42,14 +46,28 @@ void Trees_RDF_GetEfficiency(TString InputFileName = "../output/A1.8_18qr_local/
         return Hash_Centrality[std::make_tuple(RunNumber, DirNumber, EventNumber)];
     };
 
+    /* Radius */
+
+    std::map<std::tuple<Int_t, Int_t, Int_t, Int_t>, Float_t> Hash_Radius;
+    for (Int_t i = 0; i < (Int_t)MCParticles_Xv_i.size(); i++) {
+        Hash_Radius[std::make_tuple(MCParticles_RunNumber[i], MCParticles_DirNumber[i], MCParticles_EventNumber[i], MCParticles_ReactionID[i])] =
+            (Float_t)TMath::Sqrt((Double_t)MCParticles_Xv_i[i] * (Double_t)MCParticles_Xv_i[i] +
+                                 (Double_t)MCParticles_Yv_i[i] * (Double_t)MCParticles_Yv_i[i]);
+    }
+
+    auto GetRadius = [&Hash_Radius](Int_t RunNumber, Int_t DirNumber, Int_t EventNumber, Int_t ReactionID) {
+        return Hash_Radius[std::make_tuple(RunNumber, DirNumber, EventNumber, ReactionID)];
+    };
+
     /* Extend trees */
 
     auto eRDF_Injected = RDF_Injected
-                             .Define("Pt", "TMath::Sqrt(Px*Px + Py*Py)")  //
-                             .Define("Centrality", GetCentrality, {"RunNumber", "DirNumber", "EventNumber"});
-    auto eRDF_Sexaquarks = RDF_Sexaquarks                                   //
-                               .Define("Pt", "TMath::Sqrt(Px*Px + Py*Py)")  //
-                               .Define("Radius", "TMath::Sqrt(SV_Xv*SV_Xv + SV_Yv*SV_Yv)")
+                             .Define("Pt", "TMath::Sqrt(Px*Px + Py*Py)")                                      //
+                             .Define("Centrality", GetCentrality, {"RunNumber", "DirNumber", "EventNumber"})  //
+                             .Define("Radius", GetRadius, {"RunNumber", "DirNumber", "EventNumber", "ReactionID"});
+    auto eRDF_Sexaquarks = RDF_Sexaquarks
+                               .Define("Pt", "TMath::Sqrt(Px*Px + Py*Py)")                  //
+                               .Define("Radius", "TMath::Sqrt(SV_Xv*SV_Xv + SV_Yv*SV_Yv)")  //
                                .Define("Centrality", GetCentrality, {"RunNumber", "DirNumber", "EventNumber"});
 
     /* Extend trees */
@@ -60,8 +78,7 @@ void Trees_RDF_GetEfficiency(TString InputFileName = "../output/A1.8_18qr_local/
     TH1D *Hist_EfficiencyPt = new TH1D("EfficiencyPt", ";p_{T} (GeV/c);Efficiency", 100, 0., 10.);
     Hist_EfficiencyPt->Divide(Hist_RecPt.GetPtr(), Hist_TruePt.GetPtr(), 1., 1., "B");
 
-    auto Hist_Centrality = eRDF_Sexaquarks.Filter("Centrality < 90.").Filter("IsSignal").Histo1D({"Centrality", ";;", 10, 0., 100.}, "Centrality");
-
+    auto Hist_TrueRadius = eRDF_Injected.Filter("Centrality < 90.").Histo1D({"TrueRadius", ";;", 100, 0., 200.}, "Radius");
     auto Hist_RecRadius = eRDF_Sexaquarks.Filter("Centrality < 90.").Filter("IsSignal").Histo1D({"RecRadius", ";;", 100, 0., 200.}, "Radius");
 
     /* Output */
@@ -70,8 +87,10 @@ void Trees_RDF_GetEfficiency(TString InputFileName = "../output/A1.8_18qr_local/
 
     Hist_TruePt->Write();
     Hist_RecPt->Write();
+
     Hist_EfficiencyPt->Write();
-    Hist_Centrality->Write();
+
+    Hist_TrueRadius->Write();
     Hist_RecRadius->Write();
 
     /* Print how many event-loops were executed */
