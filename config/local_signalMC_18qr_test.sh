47d461a1afeec0965b8176103bf994ff0afc38a2
A. Bórquez <andres.borquezc@gmail.com>
2025-03-09 19:31:40 +0100

    started new branch by cleaning unnecessary things

--- config/local_signalMC_18qr_test.sh
+++ config/local_signalMC_18qr_test.sh
@@ -2,18 +2,17 @@
 
 # IMPORTANT: this file should always be sourced by another script
 
-if [[ -z ${SOURCE_OF_V0S} ]]; then echo "ERROR :: config_file.sh :: Missing env. var. SOURCE_OF_V0S" ; exit 1; fi
-if [[ -z ${SIMULATION_SET} ]]; then echo "ERROR :: config_file.sh :: Missing env. var. SIMULATION_SET" ; exit 1; fi
-if [[ -z ${SIMS_DIR} ]]; then echo "ERROR :: config_file.sh :: Missing env. var. SIMS_DIR" ; exit 1; fi
-if [[ -z ${ANALYSIS_DIR} ]]; then echo "ERROR :: config_file.sh :: Missing env. var. ANALYSIS_DIR" ; exit 1; fi
+if [[ -z ${LOCAL_SIMS_DIR} ]]; then echo "ERROR :: config_file.sh :: Missing env. var. LOCAL_SIMS_DIR" ; exit 1; fi
 
-export ATTEMPT_NAME="local_signalMC_18qr_test_${SOURCE_OF_V0S}${SIMULATION_SET}"
 export MODE="local"
-export LOCAL_INPUT_PATH="${SIMS_DIR}"
-export LOCAL_N_DIRS=6
-export GRID_TEST_MODE=0
-export IS_MC=1
 export PRODUCTION_NAME="LHC23l1a3"
-export RUN_NUMBERS_LIST="${ANALYSIS_DIR}/doc/LHC18qr_pass3_rn_TEST.txt"
-export DO_QA=1
-export CHOOSE_N_EVENTS=0
+export SIMULATION_SET="A1.8"
+export ATTEMPT_NAME="local_signalMC_${SIMULATION_SET}_18qr_test"
+export INPUT_PATH="${LOCAL_SIMS_DIR}/${PRODUCTION_NAME}/${SIMULATION_SET}"
+export RUN_NUMBER=295585
+export LOCAL_N_DIRS=2
+export LOCAL_LIMIT_N_EVENTS=10
+export GRID_TEST_MODE=0 # not used
+export GRID_WORKING_DIR="" # not used
+export GRID_CUSTOM_SPLIT=0 # not used
+export GRID_CUSTOM_PATTERN="" # not used
--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,59 +1,48 @@
 #!/bin/bash
 
-if [[ $# -ne 3 ]]; then
-    echo "ERROR :: exec.sh :: USAGE is \`./exec.sh <CONFIG_FILE> <SOURCE_OF_V0S> <SIMULATION_SET>\`"
-    exit 1
-fi
-
+function print_usage { echo "USAGE: bash exec.sh <CONFIG_FILE>"; }
+if [[ $# -ne 1 ]]; then print_usage; exit 1; fi
 CONFIG_FILE=$1
-export SOURCE_OF_V0S=$2
-export SIMULATION_SET=$3
 
 source ${CONFIG_FILE}
-echo "INFO :: exec.sh :: CONFIG_FILE      = ${CONFIG_FILE}"
-echo "INFO :: exec.sh :: ATTEMPT_NAME     = ${ATTEMPT_NAME}"
-echo "INFO :: exec.sh :: MODE             = ${MODE}"
-echo "INFO :: exec.sh :: LOCAL_INPUT_PATH = ${LOCAL_INPUT_PATH}"
-echo "INFO :: exec.sh :: LOCAL_N_DIRS     = ${LOCAL_N_DIRS}"
-echo "INFO :: exec.sh :: GRID_TEST_MODE   = ${GRID_TEST_MODE}"
-echo "INFO :: exec.sh :: IS_MC            = ${IS_MC}"
-echo "INFO :: exec.sh :: PRODUCTION_NAME  = ${PRODUCTION_NAME}"
-echo "INFO :: exec.sh :: RUN_NUMBERS_LIST = ${RUN_NUMBERS_LIST}"
-echo "INFO :: exec.sh :: SOURCE_OF_V0S    = ${SOURCE_OF_V0S}"
-echo "INFO :: exec.sh :: SIMULATION_SET   = ${SIMULATION_SET}"
-echo "INFO :: exec.sh :: DO_QA            = ${DO_QA}"
-echo "INFO :: exec.sh :: CHOOSE_N_EVENTS  = ${CHOOSE_N_EVENTS}"
+echo "INFO  :: exec.sh :: CONFIG_FILE          = ${CONFIG_FILE}"
+echo "INFO  :: exec.sh :: ATTEMPT_NAME         = ${ATTEMPT_NAME}"
+echo "INFO  :: exec.sh :: MODE                 = ${MODE}"
+echo "INFO  :: exec.sh :: INPUT_PATH           = ${INPUT_PATH}"
+echo "INFO  :: exec.sh :: PRODUCTION_NAME      = ${PRODUCTION_NAME}"
+echo "INFO  :: exec.sh :: SIMULATION_SET       = ${SIMULATION_SET}"
+echo "INFO  :: exec.sh :: RUN_NUMBER           = ${RUN_NUMBER}"
+echo "INFO  :: exec.sh :: LOCAL_N_DIRS         = ${LOCAL_N_DIRS}"
+echo "INFO  :: exec.sh :: LOCAL_LIMIT_N_EVENTS = ${LOCAL_LIMIT_N_EVENTS}"
+echo "INFO  :: exec.sh :: GRID_TEST_MODE       = ${GRID_TEST_MODE}"
+echo "INFO  :: exec.sh :: GRID_WORKING_DIR     = ${GRID_WORKING_DIR}"
+echo "INFO  :: exec.sh :: GRID_CUSTOM_SPLIT    = ${GRID_CUSTOM_SPLIT}"
+echo "INFO  :: exec.sh :: GRID_CUSTOM_PATTERN  = ${GRID_CUSTOM_PATTERN}"
 
 ATTEMPT_DIR=attempts/${ATTEMPT_NAME}
 mkdir -p ${ATTEMPT_DIR}
 
-cp AliAnalysisTaskSexaquark.cxx ${ATTEMPT_DIR}/
-cp AliAnalysisTaskSexaquark.h ${ATTEMPT_DIR}/
-cp AddSexaquark.C ${ATTEMPT_DIR}/
+cp AliAnalysisTaskSexaquark* ${ATTEMPT_DIR}/
+cp AddTaskSexaquark.C ${ATTEMPT_DIR}/
 cp runAnalysis.C ${ATTEMPT_DIR}/
 
 cd ${ATTEMPT_DIR}
 
 ANALYSIS_OPTIONS="("
 ANALYSIS_OPTIONS+="\"${MODE}\","
-ANALYSIS_OPTIONS+="\"${LOCAL_INPUT_PATH}\","
-ANALYSIS_OPTIONS+="${LOCAL_N_DIRS},"
-ANALYSIS_OPTIONS+="${GRID_TEST_MODE},"
-ANALYSIS_OPTIONS+="${IS_MC},"
+ANALYSIS_OPTIONS+="\"${INPUT_PATH}\","
 ANALYSIS_OPTIONS+="\"${PRODUCTION_NAME}\","
-ANALYSIS_OPTIONS+="\"${RUN_NUMBERS_LIST}\","
-ANALYSIS_OPTIONS+="\"${SOURCE_OF_V0S}\","
-ANALYSIS_OPTIONS+="\"${SIMULATION_SET}\","
-ANALYSIS_OPTIONS+="${DO_QA},"
-ANALYSIS_OPTIONS+="${CHOOSE_N_EVENTS}"
+ANALYSIS_OPTIONS+="${RUN_NUMBER},"
+ANALYSIS_OPTIONS+="${LOCAL_N_DIRS},"
+ANALYSIS_OPTIONS+="${LOCAL_LIMIT_N_EVENTS},"
+ANALYSIS_OPTIONS+="${GRID_TEST_MODE},"
+ANALYSIS_OPTIONS+="\"${GRID_WORKING_DIR}\","
+ANALYSIS_OPTIONS+="${GRID_CUSTOM_SPLIT},"
+ANALYSIS_OPTIONS+="\"${GRID_CUSTOM_PATTERN}\""
 ANALYSIS_OPTIONS+=")"
 
 ALIROOT_COMMAND='aliroot -l -b -q runAnalysis.C'${ANALYSIS_OPTIONS}''
 echo ${ALIROOT_COMMAND}
 ${ALIROOT_COMMAND} 2>&1 | tee analysis.log
-
-rm -v AliAnalysisTaskSexaquark*
-rm -v AddSexaquark.C
-rm -v runAnalysis.C
 
 cd ..
